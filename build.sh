0d40f0eb92d9ab6a2e5b6bb2fefb96df31990c62
omkaryaaws <162447629+omkaryaaws@users.noreply.github.com>
2024-04-28 20:43:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 VERSION=$(date +%H-%M-%S)
 docker build -t omkaryacool/testingbuild:${VERSION} .
 docker push omkaryacool/testingbuild:${VERSION}
-docker -H tcp://172.31.9.244:2375 stop nginx
-docker -H tcp://172.31.9.244:2375 run --rm -dit -p 8000:80 --name nginx --hostname nginx omkaryacool/testingbuild:${VERSION}
+docker -H tcp://172.31.6.74:2375 stop nginx
+docker -H tcp://172.31.6.74:2375 run --rm -dit -p 8000:80 --name nginx --hostname nginx omkaryacool/testingbuild:${VERSION}
 echo "---------------------"
